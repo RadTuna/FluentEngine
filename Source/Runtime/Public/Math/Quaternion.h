3458bf73f238ce2333e92f9022003701b6a19964
Jeon JaeSeung <wotmd6180@gmail.com>
2020-02-27 13:48:50 +0900

    Quaternion header definition

--- Source/Runtime/Public/Math/Quaternion.h
+++ Source/Runtime/Public/Math/Quaternion.h
@@ -1,1 +1,46 @@
 #pragma once
+
+
+namespace Fluent
+{
+	class Vector3;
+	class Vector4;
+	
+	class Quaternion
+	{
+	public:
+
+		// Real number
+		float mR;
+
+		// Image number
+		float mI;
+		float mJ;
+		float mK;
+
+	public:
+
+
+		Quaternion operator+(const Quaternion& other);
+		Quaternion operator+=(const Quaternion& other);
+
+		Quaternion operator-(const Quaternion& other);
+		Quaternion operator-=(const Quaternion& other);
+
+		Quaternion operator*(const Quaternion& other);
+		Quaternion operator*=(const Quaternion& other);
+
+		Vector3 Rotate(const Vector3& other);
+		Vector4 Rotate(const Vector4& other);
+
+		void SetEuler(const Vector3& other);
+
+		[[nodiscard]]
+		Vector3 ToEuler() const;
+		
+		[[nodiscard]]
+		float Length() const;
+
+	};
+
+}
